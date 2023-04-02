a008533d3dbd57ea6eb65d513e2474b067e38d02
Elgania Aulia Gemintang <elganiaaulia02@gmail.com>
2023-04-02 23:01:57 +0700

    120140113

---
+++ data/universitas_1.js
@@ -0,0 +1,19 @@
+var json_universitas_1 = {
+"type": "FeatureCollection",
+"name": "universitas_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nama": "teknokrat", "foto": "C:\\Users\\user\\Documents\\ITERA\\Semester6\\sig\\foto kampus\\teknokrat.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.257780987181675, -5.382376133059407 ] } },
+{ "type": "Feature", "properties": { "nama": "bandar lampung university", "foto": "C:\\Users\\user\\Documents\\ITERA\\Semester6\\sig\\foto kampus\\bandar lampung university.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.251950765686018, -5.378982086070113 ] } },
+{ "type": "Feature", "properties": { "nama": "Universitas Muhammadiyah Lampung", "foto": "C:\\Users\\user\\Documents\\ITERA\\Semester6\\sig\\foto kampus\\Universitas Muhammadiyah Lampung.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.252725535706134, -5.377415658885399 ] } },
+{ "type": "Feature", "properties": { "nama": "Institut Teknologi Sumatera", "foto": "C:\\Users\\user\\Documents\\ITERA\\Semester6\\sig\\foto kampus\\Institut Teknologi Sumatera.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.314841017022545, -5.358220709756661 ] } },
+{ "type": "Feature", "properties": { "nama": "Kampus UIN", "foto": "C:\\Users\\user\\Documents\\ITERA\\Semester6\\sig\\foto kampus\\Kampus UIN ril.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.304806965906465, -5.378642468986002 ] } },
+{ "type": "Feature", "properties": { "nama": "Informatics & Business Institute Darmajaya", "foto": "C:\\Users\\user\\Documents\\ITERA\\Semester6\\sig\\foto kampus\\Informatics & Business Institute Darmajaya.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.249733529108454, -5.377438917749509 ] } },
+{ "type": "Feature", "properties": { "nama": "Universitas Mitra Indonesia", "foto": "C:\\Users\\user\\Documents\\ITERA\\Semester6\\sig\\foto kampus\\Universitas Mitra Indonesia.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.241598749985371, -5.373075690635845 ] } },
+{ "type": "Feature", "properties": { "nama": "Sekolah Tinggi Perkebunan Lampung", "foto": "C:\\Users\\user\\Documents\\ITERA\\Semester6\\sig\\foto kampus\\Sekolah Tinggi Perkebunan Lampung.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.24252759347894, -5.374156327795292 ] } },
+{ "type": "Feature", "properties": { "nama": "UT Bandar Lampung", "foto": "C:\\Users\\user\\Documents\\ITERA\\Semester6\\sig\\foto kampus\\UT Bandar Lampung.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.24576716622218, -5.375832311897232 ] } },
+{ "type": "Feature", "properties": { "nama": "Sekokah Tinggi Ilmu Kesehatan Panca Bhakti", "foto": "C:\\Users\\user\\Documents\\ITERA\\Semester6\\sig\\foto kampus\\Sekokah Tinggi Ilmu Kesehatan Panca Bhakti.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.248174664062745, -5.375952054481764 ] } },
+{ "type": "Feature", "properties": { "nama": "Lampung University", "foto": "C:\\Users\\user\\Documents\\ITERA\\Semester6\\sig\\foto kampus\\Lampung University.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.242904301617585, -5.3641364614689 ] } },
+{ "type": "Feature", "properties": { "nama": "Lampung State Polytechnic", "foto": "C:\\Users\\user\\Documents\\ITERA\\Semester6\\sig\\foto kampus\\Lampung State Polytechnic.jpg" }, "geometry": { "type": "Point", "coordinates": [ 105.232936010793154, -5.35841000573024 ] } }
+]
+}
